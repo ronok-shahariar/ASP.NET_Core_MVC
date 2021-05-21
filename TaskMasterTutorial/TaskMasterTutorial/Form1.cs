bdeb9694d97e70bff872f0766223c58eb3ea05aa
ronok-shahariar <346ronokarya@gmail.com>
2021-05-21 12:44:00 +0600

    UI design

--- TaskMasterTutorial/TaskMasterTutorial/Form1.cs
+++ TaskMasterTutorial/TaskMasterTutorial/Form1.cs
@@ -17,5 +17,14 @@ namespace TaskMasterTutorial
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
